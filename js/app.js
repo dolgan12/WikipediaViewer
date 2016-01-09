32f8859547dc33589bcb6dec081526b74fae4ecd
Conny Garpö <dolgan12@gmail.com>
2016-01-09 23:32:45 +0100

    Added animations

--- js/app.js
+++ js/app.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
+
+
+    var api = 'http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var callback = '&callback=JSON_CALLBACK';
+
+
+
             var submitIcon = $('.searchbox-icon');
             var inputBox = $('.searchbox-input');
             var searchBox = $('.searchbox');
+            var rubrik = $('.rubrik');
             var isOpen = false;
             submitIcon.click(function(){
                 if(isOpen === false){
                     searchBox.addClass('searchbox-open');
+                    searchBox.animate({'right' : "-=275px"}, 200);
+                    rubrik.delay(900).slideUp(500);
                     inputBox.focus();
                     isOpen = true;
                 } else {
                     searchBox.removeClass('searchbox-open');
                     inputBox.focusout();
+                    searchBox.delay(700).animate({'right' : "+=275px"}, 200);
+                    rubrik.delay(1000).slideDown(500);
                     isOpen = false;
                 }
             });
@@ -27,13 +39,13 @@ $(document).ready(function(){
                     }
                 });
         });
-            function buttonUp(){
-                var inputVal = $('.searchbox-input').val();
-                inputVal = $.trim(inputVal).length;
-                if( inputVal !== 0){
-                    $('.searchbox-icon').css('display','none');
-                } else {
-                    $('.searchbox-input').val('');
-                    $('.searchbox-icon').css('display','block');
-                }
-            }
+function buttonUp(){
+  var inputVal = $('.searchbox-input').val();
+  inputVal = $.trim(inputVal).length;
+  if( inputVal !== 0){
+    $('.searchbox-icon').css('display','none');
+    } else {
+      $('.searchbox-input').val('');
+      $('.searchbox-icon').css('display','block');
+      }
+}
